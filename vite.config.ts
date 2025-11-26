8b4e1d9a025755f16914683f1765b8c9bad3899e
thenmozhiloganathan <thenmozhiloganathan18@gmail.com>
2025-11-26 11:57:14 +0530

    v3

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/Ai-Mental-Health-Analyzer/",  // ⭐ REQUIRED for GitHub Pages
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ["lucide-react"],
   },
 });
